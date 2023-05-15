db0a2721e219fc279097b2e369e30a3d494bda39
MGPKSDUK <132699072+MGPKSDUK@users.noreply.github.com>
2023-05-15 08:26:26 +0530

    Add files via upload

---
+++ sketch_may5a123.ino
@@ -0,0 +1,35 @@
+#include <Servo.h>
+Servo m;
+void setup() {
+  // put your setup code here, to run once:
+ m.attach(10);
+ Serial.begin(9600);
+ pinMode(5,INPUT);
+
+}
+int n=0;
+void loop() {
+  // put your main code here, to run repeatedly:
+  int x=digitalRead(5);
+  Serial.println(x);
+  Serial.println("Enter:");
+  
+  Serial.println(n);
+  if((x == 0) && (n == 0)){
+ for(int i = 0;i<=180;i=i+20){
+   m.write(i);
+   delay(500);
+   
+ }
+ n++;
+  }
+ else if((x != 0) && (n == 1)){
+ for(int i = 180;i>=0;i=i-20){
+   m.write(i);
+   delay(500);
+   
+ }
+  n--;
+ }
+ delay(10000);
+}
